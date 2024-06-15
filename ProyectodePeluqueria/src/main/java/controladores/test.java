4a77635daa8aa534b892a6f52980fc7403ae62e1
lh20004 <164256419+lh20004@users.noreply.github.com>
2024-06-15 16:28:28 -0600

    incluision_archivo

---
+++ ProyectodePeluqueria/src/main/java/controladores/test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controladores;
+
+/**
+ *
+ * @author javier
+ */
+public class test {
+    private static void main(String args[]){
+        System.out.print("javier");
+    }
+}
