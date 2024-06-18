17dd474efdd22bd4e21e2f3f153c7202519c7fa9
Richardl24 <164256575+Richardl24@users.noreply.github.com>
2024-06-18 16:22:19 -0600

    Update Reserva.java

--- ProyectodePeluqueria/src/main/java/modelo/Reserva.java
+++ ProyectodePeluqueria/src/main/java/modelo/Reserva.java
@@ -22,7 +22,7 @@ import lombok.Setter;
 @Setter
 public class Reserva {
    private int idReserva;
-   private int idServicio;
+   private int idServicio;//No utilizar 
    private Date fechaReserva;
    private Time horaInicio;
    private Time horaFin;
