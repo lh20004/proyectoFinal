dfe8cc3b3c6bb9df39468c9d749237d4a262d49d
Phaser02 <lm22026@ues.edu.sv>
2024-06-18 20:27:36 -0600

    Crud, consultas a modificar?

---
+++ ProyectodePeluqueria/src/main/java/ControllersConsultas/GananciasDia.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ControllersConsultas;
+
+import dao.ServiciosDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Servicio;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+@WebServlet(name = "GananciasDiaServlet", urlPatterns = {"/GananciasDiaServlet"})
+public class GananciasDia extends HttpServlet{
+    ArrayList<Servicio> listaServicio;
+    Servicio au=null;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         resp.setContentType("aplication/json;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        String filtro = req.getParameter("consultar_datos");
+
+        System.out.println(filtro);
+        if (filtro == null) {
+            return;
+        }
+
+        switch (filtro) {
+            
+
+            case "si_consulta":
+                JSONArray array_autor = new JSONArray();
+                JSONObject json_autor = new JSONObject();
+                String html = "";
+                String el_estado = req.getParameter("estado");
+
+                try {
+                    ServiciosDAO obaut = new ServiciosDAO();
+                    this.listaServicio = new ArrayList();
+
+                    this.listaServicio = obaut.obtenerGananciasDia(Integer.valueOf(el_estado), "todo");
+
+                    System.out.println("Esta en resultado en RegAutor");
+
+                    html += "<table id=\"tabla_ganancias\""
+                            + "class=\"table table-bordered dt-resposive nowrap\""
+                            + "cellspacing=\"0\" width=\"100%\">\n"
+                            + "<thead>\n"
+                            + "<tr>\n"
+                            
+                           
+                            + "<th>Ganancias </th>\n"
+                           
+                            + "</tr>\n"
+                            + "</thead>\n"
+                            + "</tbody>";
+
+                    int cont = 0;
+                    for (Servicio objAutor : this.listaServicio) {
+                        cont++;
+                        html += "<tr>";
+                        
+                        html += "<td>" + objAutor.getTotalganancias() + "</td>";
+                       
+
+                        html += "<td>";
+                        
+                        html += "</td>";
+                        html += "</tr>";
+                    }
+                    html += "</tbody\n>"
+                            + "\t\t </table>";
+                    json_autor.put("resultado", "exito");
+                    json_autor.put("tabla", html);
+                    json_autor.put("cuanto", cont);
+                    System.out.println("que tiene" + html);
+
+                } catch (SQLException e) {
+
+                    json_autor.put("resultado", "error sql");
+                    json_autor.put("error", e.getMessage());
+                    json_autor.put("code error", e.getErrorCode());
+                    throw new RuntimeException(e);
+
+                }
+                array_autor.put(json_autor);
+                resp.getWriter().write(array_autor.toString());
+                break;
+
+            
+
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+    
+    
+    
+}
--- ProyectodePeluqueria/src/main/java/modelo/Servicio.java
+++ ProyectodePeluqueria/src/main/java/modelo/Servicio.java
@@ -14,16 +14,131 @@ import lombok.Setter;
  *
  * @author guill
  */
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+
 public class Servicio {
     private int idServicio;
     private String servicio;
     private String descripcion;
     private double precio;
+    private String estado;
+    private int totalServicios;
+    private double totalganancias;
     private ArrayList<Reserva> listaReserva;
     private ArrayList<Pago> listaPago;
+    
+    public Servicio() {
+    }
+
+    public Servicio( double totalganancias) {
+     
+        this.totalganancias = totalganancias;
+    }
+
+    public double getTotalganancias() {
+        return totalganancias;
+    }
+
+    public void setTotalganancias(double totalganancias) {
+        this.totalganancias = totalganancias;
+    }
+    
+
+    public Servicio(int idServicio, String servicio, String descripcion, double precio, String estado, ArrayList<Reserva> listaReserva, ArrayList<Pago> listaPago) {
+        this.idServicio = idServicio;
+        this.servicio = servicio;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.estado = estado;
+        this.listaReserva = listaReserva;
+        this.listaPago = listaPago;
+    }
+
+    public Servicio(String servicio, String descripcion, double precio, String estado) {
+        this.servicio = servicio;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public Servicio(int idServicio, String servicio, String descripcion, double precio, String estado) {
+        this.idServicio = idServicio;
+        this.servicio = servicio;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public Servicio(String servicio, int totalServicios) {
+        this.servicio = servicio;
+        this.totalServicios = totalServicios;
+    }
+
+    public int getTotalServicios() {
+        return totalServicios;
+    }
+
+    public void setTotalServicios(int totalServicios) {
+        this.totalServicios = totalServicios;
+    }
+    
+    
+    
+    
+
+    public int getIdServicio() {
+        return idServicio;
+    }
+
+    public void setIdServicio(int idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public String getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public ArrayList<Reserva> getListaReserva() {
+        return listaReserva;
+    }
+
+    public void setListaReserva(ArrayList<Reserva> listaReserva) {
+        this.listaReserva = listaReserva;
+    }
+
+    public ArrayList<Pago> getListaPago() {
+        return listaPago;
+    }
+
+    public void setListaPago(ArrayList<Pago> listaPago) {
+        this.listaPago = listaPago;
+    }
     
 }
